94db1c25441bc70848e63bcdf9c067209c2e997f
Olivier Auberger <50233804+olive89aja@users.noreply.github.com>
2019-08-24 14:16:43 -0500

    Add files via upload
    First 10 results of search displayed. Still working on small images display.

---
+++ nytapp.js
@@ -0,0 +1,41 @@
+
+$(document).ready(() => {
+    $("#submit").click((e) => {
+        e.preventDefault();
+        const query = $("#search").val();
+        const startyr = $("#startYr").val();
+        const endyr = $("#endYr").val();
+        const records = $("#records").val();
+        let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=PxcY2DIl1gMaBtEvjfHuFRF9vEXgLaKi"
+        if(startyr) {
+            url+="&begin_date="+startyr+"0101";
+        }
+        if(endyr) {
+            url+="&end_date="+endyr+"1231";
+        }
+        $.ajax(url).then(
+        (a) => {
+            console.log(a);
+            for(i = 0; i < 10; i++) {
+                currentArticle = a.response.docs[i];
+                const article = $("<div>");
+                //const thumbnail = $("<img>");
+                const headline = $("<h4>");
+                const link = $("<a>");
+                const byline = $("<p>");
+                const snippet = $("<p>");
+                link.text(currentArticle.headline.main);
+                link.attr("href", currentArticle.web_url);
+                headline.append(link);
+                byline.text(currentArticle.byline.original);
+                snippet.text(currentArticle.snippet);
+                //thumbnail.attr("src", "https://nytimes.com/"+currentArticle.multimedia[16].url);
+                //article.append(thumbnail);
+                article.append(headline);
+                article.append(byline);
+                article.append(snippet);
+                $("#toparticles").append(article);
+            }
+        });
+    });            
+});
